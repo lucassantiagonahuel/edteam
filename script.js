562d46cc946239ebe2de3b0943762f8f4240db84
lucasnahuel <lucassantiagonahuel@gmail.com>
2020-06-02 03:44:19 -0300

    edteamprueba

---
+++ script.js
@@ -0,0 +1,75 @@
+/*let edad = 4
+let nombre = `Lucas tiene :
+ ${edad} piernas`
+ 
+
+let div = `<div class=container>
+             ${nombre}
+            </div>
+            `
+let edades = [1,2,3,4,5]
+let data= {
+      nombre :'Juan',
+      apellido:'Perez',
+      edad:90
+}      
+console.log(typeof edades) 
+console.log(typeof data)     
+document.body.innerHTML = div  
+let user='Juan'
+let edad = prompt('Ingresa tu edad')
+alert (edad)
+let age=prompt('Ingrese su edad')
+let isadult= age>=18
+               ? 'Eres adulto'
+               : 'Eres menor'
+alert(isadult)
+let a
+let b= a && 'Pone algo'  
+
+let number= 12.452665532155
+console.log(number.toFixed(3))
+
+let texto = '15'
+let textopasadoanumero = parseInt(texto,10)
+
+let texto = '12.45'
+let textopasadoanumero= parseFloat(texto)
+
+console.log(Math.ceil(Math.random() * 50))
+
+let alumno= 'Lucas Nahuel'
+console.log(alumno.length)
+
+let consulta = prompt('¿Cual es la capital de argentina?').toUpperCase().trim()
+let mensaje = consulta === 'BUENOS AIRES'
+                      ? 'La respuesta es correcta'
+                      : '¡Respuesta equivocada! Es Buenos Aires'
+    alert(mensaje)       
+    let nombre = 'Lucas'             
+  console.log(nombre.indexOf('a'))
+
+let nombre= 'lucas.com'
+console.log(nombre.includes('com'))  
+
+let pagina = 'https/lucas/rock'
+console.log(pagina.startsWith('https/lucas'))
+
+let pagina = 'https/lucas/rock'
+console.log(pagina.endsWith('https/lucas/rock'))
+
+let palabras= 'Hola carola'
+console.log(palabras.replace('carola','carolas'))
+
+let palabras= 'Hola carola'
+console.log(palabras.split('')[5])
+
+let palabras= 'Hola carola'
+console.log(palabras.substring(1,3))
+
+let palabras = 'Hola carola'
+console.log(palabras.substr(1,6))
+
+let palabras= 'Hola carola'
+console.log(palabras.slice(-6,-2))*/
+
